1efb4168a70ceefc7e7100002e06bfcf7458bd52
Alice LIN <alilin@e2r10p10.42.fr>
2021-09-21 12:18:35 +0200

    re

---
+++ utils/enable_if.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   enable_if.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alilin <alilin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/21 11:17:39 by alilin            #+#    #+#             */
+/*   Updated: 2021/09/21 11:45:14 by alilin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ENABLE_IF_HPP
+# define ENABLE_IF_HPP
+
+namespace ft
+{
+	template <class T>
+	struct enable_if<true, T> { typedef T type; };
+}
+
+#endif
