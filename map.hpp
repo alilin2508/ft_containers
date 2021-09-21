dc59936cab6ba9afde05290b08f11dc36851cd45
alilin2508 <alice.lin2508@hotmail.com>
2021-09-21 16:34:50 +0200

    started map

---
+++ map.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alilin <alilin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/21 16:09:38 by alilin            #+#    #+#             */
+/*   Updated: 2021/09/21 16:28:45 by alilin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAP_HPP
+# define MAP_HPP
+
+#include "./utils/pair.hpp"
+
+#include <functional>
+#include <memory>
+
+namespace ft
+{
+	template < class Key, class T, class Compare = std::less<Key>, class Alloc = std::allocator<ft::pair<const Key,T> > >
+	class map
+	{
+
+	};
+}
+
+#endif
