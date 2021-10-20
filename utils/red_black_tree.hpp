ac8a696a6410c817c4a894aefd391934fb4ac8b7
Alice LIN <alilin@e1r8p7.42.fr>
2021-10-20 15:35:36 +0200

    RBtree deleting nodes

--- utils/red_black_tree.hpp
+++ utils/red_black_tree.hpp
@@ -6,7 +6,7 @@
 /*   By: alilin <alilin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 11:51:15 by alilin            #+#    #+#             */
-/*   Updated: 2021/10/19 15:43:56 by alilin           ###   ########.fr       */
+/*   Updated: 2021/10/20 15:35:01 by alilin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,22 +37,23 @@ namespace ft
 		value_type data;
 		color color;
 
-		// Node(value_type data, Node* parent, Node* left, Node* right,calor color): data(data), parent(parent), left(left), right(right), color(color) {}
+		Node(value_type data, Node* parent, Node* left, Node* right,calor color): data(data), parent(parent), left(left), right(right), color(color) {}
 	};
 
-	template <class Key, class T, class Compare = std::less<Key> >
+	template <class Key, class T, class Compare = std::less<Key>, >
 	class RBtree
 	{
 	public:
 
 		typedef T value_type;
+		typedef Key key_type;
 		typedef Node<value_type>* node_ptr;
 		typedef Compare Key_compare;
 
 		RBTree()
 		{
 			_nil = new Node;
-			_nil->color = 0;
+			_nil->color = black;
 			_nil->left = NULL;
 			_nil->right = NULL;
 			root = _nil;
@@ -61,6 +62,22 @@ namespace ft
 		node_pointer getRoot()
 		{
 			return (this->_root);
+		}
+
+		// find the node with the minimum key
+		node_ptr minimum(node_ptr node)
+		{
+			while (node->left != TNULL)
+				node = node->left;
+			return node;
+		}
+
+		// find the node with the maximum key
+		node_ptr maximum(node_ptr node)
+		{
+			while (node->right != TNULL)
+				node = node->right;
+			return node;
 		}
 
 		void left_rotate(node_ptr x)
@@ -144,6 +161,11 @@ namespace ft
 			fixInsert(node);
 		}
 
+		void deleteNode(key_type key)
+		{
+			deleteNodeHelper(this->_root, key);
+		}
+
 	protected:
 
 		node_ptr _root;
@@ -172,13 +194,12 @@ namespace ft
 							z = z->parent;
 							right_rotate(z); // new z is old parent
 						}
-						// case 3.2.1
 						z->parent->color = black;
 						z->parent->parent->color = red;
 						left_rotate(z->parent->parent);
 					}
 				}
-				else // parent is gp's left child
+				else // parent is gp's left child #mirror_case
 				{
 					u = z->parent->parent->right; // uncle
 					if (u->color == red)
@@ -209,6 +230,73 @@ namespace ft
 				}
 			}
 			root->color = black;
+		}
+
+		void rbTransplant(node_ptr u, node_ptr v) // replaces u by v
+		{
+			if (u->parent == NULL)
+				root = v;
+			else if (u == u->parent->left)
+				u->parent->left = v;
+			else
+				u->parent->right = v;
+			v->parent = u->parent;
+		}
+
+		void deleteNodeHelper(node_ptr node, key_type key)
+		{
+			// find the node containing key
+			node_ptr z = NULL;
+			node_ptr x, y;
+			while (node != NULL)
+			{
+				if (get_key_from_val(node->data) == key)
+					z = node;
+				if (get_key_from_val(node->data) <= key)
+					node = node->right;
+			 	else
+					node = node->left;
+			}
+
+			if (z == NULL)
+			{
+				std::cout << "Couldn't find key in the tree" << std::endl;
+				return;
+			}
+
+			y = z; // y saves the suppressed node's placement
+			color y_og_color = y->color;
+			if (z->left == NULL) // z only had 1 child whitch is the right one so so it get's replaced by it's child
+			{
+				x = z->right; // x saves the right child's branch
+				rbTransplant(z, z->right);
+			}
+			else if (z->right == NULL) //mirror case
+			{
+				x = z->left;
+				rbTransplant(z, z->left);
+			}
+			else // suppressed node had 2 children and is replaced by the minimum of it's right branch
+			{
+				y = minimum(z->right); // search for the minimum in the right child's branch
+				y_og_color = y->color;
+				x = y->right; // x saves the minimum's right branch
+				if (y->parent == z) // the minimum is z->right
+					x->parent = y;
+				else
+				{
+					rbTransplant(y, y->right); // replaces the minimum by it's right branch
+					y->right = z->right; // set the new z's right side
+					y->right->parent = y;
+				}
+				rbTransplant(z, y); // replace z by the correct value whitch is y and maintain the tree as a good search tree
+				y->left = z->left; // set the new z's left side
+				y->left->parent = y;
+				y->color = z->color; // we keep the old z color
+			}
+			delete z;
+			if (y_og_color == black)
+				fixDelete(x);
 		}
 
 	};
